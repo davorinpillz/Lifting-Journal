1328d8795069b3a58df8eae6527eba51375387b2
davorinpillz <ddp1329@gmail.com>
2025-02-08 15:11:35 +0100

    test

--- src/backend/server.js
+++ src/backend/server.js
@@ -8,7 +8,7 @@ import Cookies from "js-cookie";
 import Workout from '../models/Workout.js'
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
